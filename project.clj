73e59663788cb476a4283282419669adac2b9da6
Lars Rune Nøstdal <larsnostdal@gmail.com>
2020-12-19 01:02:13 +0200

    Upgrade a few deps and set version to 0.2.9

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject ring-sse/ring-sse "0.2.9-SNAPSHOT"
+(defproject lnostdal/ring-sse "0.2.9"
   :description "Ring async (Spec 1.4+) Server-Sent Events handler (and helpers)"
   :url "https://github.com/bobby/ring-sse"
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.443"]
-                 [ring/ring-core "1.6.1" :scope "provided"]]
-  :profiles {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}}
+  :dependencies [[org.clojure/clojure "1.10.2-rc1"]
+                 [org.clojure/core.async "1.3.610"]
+                 [ring/ring-core "1.8.2" :scope "provided"]]
+  :profiles {:dev {:dependencies [[ring/ring-mock "0.4.0"]]}}
   :deploy-repositories [["releases" {:url           "https://clojars.org/repo"
                                      :sign-releases false
                                      :username      :env
